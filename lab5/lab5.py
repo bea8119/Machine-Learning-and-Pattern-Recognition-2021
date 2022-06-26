a0d7f4d635042d8d95c4f1d7ae951a044e63c90e
oormacheah <oormacheah@gmail.com>
2022-06-26 15:29:53 +0200

    lab5 done KFold implemented

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -3,19 +3,22 @@ import numpy as np
 import sys
 import scipy.special
 
-sys.path.append('/home/oormacheah/Desktop/Uni shit/MLPR') # for linux
-# sys.path.append('C:/Users/andre/Desktop/Cositas/poli_repo/MLPR_21-22') # for windows
+# sys.path.append('/home/oormacheah/Desktop/Uni shit/MLPR') # for linux
+sys.path.append('C:/Users/andre/Desktop/Cositas/poli_repo/MLPR_21-22') # for windows
 from lab3.lab3 import SW_compute, datasetCovarianceM, SB_compute
 from utility.vrow_vcol import vcol, vrow
 from lab2.load_plot import load
 from lab4.lab4 import logpdf_GAU_ND
 
-def split_db_2to1(D, L, seed=0):
+def split_db_2to1(D, seed=0):
     nTrain = int(D.shape[1]*2.0/3.0) # should be 50
     np.random.seed(seed)
     idx = np.random.permutation(D.shape[1]) # take a random order of indexes from 0 to N
     idxTrain = idx[0:nTrain] 
     idxTest = idx[nTrain:]
+    return idxTrain, idxTest
+
+def split_dataset(D, L, idxTrain, idxTest):
     DTR = D[:, idxTrain]
     DTE = D[:, idxTest]
     LTR = L[idxTrain]
@@ -24,9 +27,9 @@ def split_db_2to1(D, L, seed=0):
 
 # This function will return the test samples, mean and covariance matrix per class
 # Dataset, Labels, prior probabilities (to be passed in vcol shape), k number of classes
-def classifierSetup(D, L, k, splitFunction, tied=False): 
+def classifierSetup(D, L, k, idxTrain, idxTest, tied=False): 
     # Gaussian Classifier
-    (DTR, LTR), (DTE, LTE) = splitFunction(D, L) # DTRain DTEst
+    (DTR, LTR), (DTE, LTE) = split_dataset(D, L, idxTrain, idxTest) # DTRain DTEst
 
     mu_arr = [vcol(DTR[:, LTR == i].mean(axis=1)) for i in range(k)]
 
@@ -37,16 +40,19 @@ def classifierSetup(D, L, k, splitFunction, tied=False):
         C_arr = [C_tied for i in range(k)]
     return (DTE, LTE), mu_arr, C_arr 
 
-def testLabelPredAccuracy(PredictedL, LTE, classifierName):
+def testLabelPredAccuracy(PredictedL, LTE, classifierName, show):
     CorrectPred = (PredictedL == LTE) # Boolean array of predictions
     CorrectCount = sum(CorrectPred)
     accuracy = CorrectCount / LTE.shape[0]
     error_rate = 1 - accuracy
-    print('Accuracy of' + classifierName + ' Gaussian classifier: ' + str(round(accuracy * 100, 2)) + '%')
-    print('Error rate of' + classifierName + ' Gaussian classifier: ' + str(round(error_rate * 100, 2)) + '%')
+    error_count = LTE.shape[0] - CorrectCount
+    if (show):
+        # print('Accuracy of' + classifierName + ' Gaussian classifier: ' + str(round(accuracy * 100, 2)) + ' %')
+        print('Error rate of' + classifierName + ' Gaussian classifier: ' + str(round(error_rate * 100, 2)) + ' %')
+    return error_count
 
 
-def gaussianCSF(DTE, LTE, k, mu_arr, C_arr, priorP, CSF_name, log=False):
+def gaussianCSF(DTE, LTE, k, mu_arr, C_arr, priorP, CSF_name, log, show):
      # now focus on the TEST samples
     N_test = DTE.shape[1]
 
@@ -67,9 +73,9 @@ def gaussianCSF(DTE, LTE, k, mu_arr, C_arr, priorP, CSF_name, log=False):
 
         PredictedL = SPost.argmax(0) # INDEX of max through axis 0
 
-        testLabelPredAccuracy(PredictedL, LTE, CSF_name)
+        err_count = testLabelPredAccuracy(PredictedL, LTE, CSF_name, show)
 
-        return PredictedL
+        return err_count
 # ---------------------------------------
     else:
         # Now, with log densities
@@ -85,31 +91,69 @@ def gaussianCSF(DTE, LTE, k, mu_arr, C_arr, priorP, CSF_name, log=False):
         PredictedL_log = SPost_afterLog.argmax(0) # INDEX of max through axis 0
         
         CSF_name = CSF_name + ' with log'
-        testLabelPredAccuracy(PredictedL_log, LTE, CSF_name)
+        err_count = testLabelPredAccuracy(PredictedL_log, LTE, CSF_name, show)
 
-        return PredictedL_log
+        return err_count
 
-def naiveBayesGaussianCSF(DTE, LTE, k, mu_arr, C_arr, priorP, log=False):
-    C_naive_arr = [C_arr[i] * np.identity(C_arr[i].shape[0]) for i in range(3)] # element by element mult.
-    gaussianCSF(DTE, LTE, k, mu_arr, C_naive_arr, priorP, " Naive Bayes", log)
+def gaussianCSF_wrapper(D, L, k, idxTrain, idxTest, priorP, log=False, show=True):
+    (DTE, LTE), mu_arr, C_arr = classifierSetup(D, L, k, idxTrain, idxTest)
+    return gaussianCSF(DTE, LTE, k, mu_arr, C_arr, priorP, "", log, show)
 
-def tiedCovarianceGaussianCSF(D, L, k, splitFuncion, priorP, log=False):
-    (DTE, LTE), mu_arr, C_arr = classifierSetup(D, L, k, splitFuncion, tied=True)
-    gaussianCSF(DTE, LTE, k, mu_arr, C_arr, priorP, " Tied Covariance", log)
+def naiveBayesGaussianCSF(D, L, k, idxTrain, idxTest, priorP, log=False, show=True):
+    (DTE, LTE), mu_arr, C_arr = classifierSetup(D, L, k, idxTrain, idxTest)
+    C_naive_arr = [C_arr[i] * np.identity(C_arr[i].shape[0]) for i in range(k)] # element by element mult.
+    return gaussianCSF(DTE, LTE, k, mu_arr, C_naive_arr, priorP, " Naive Bayes", log, show)
+
+def tiedCovarianceGaussianCSF(D, L, k, idxTrain, idxTest, priorP, log=False, show=True):
+    (DTE, LTE), mu_arr, C_arr = classifierSetup(D, L, k, idxTrain, idxTest, tied=True)
+    return gaussianCSF(DTE, LTE, k, mu_arr, C_arr, priorP, " Tied Covariance", log, show)
+
+def tiedNaiveBayesGaussianClassifier(D, L, k, idxTrain, idxTest, priorP, log=False, show=True):
+    (DTE, LTE), mu_arr, C_arr = classifierSetup(D, L, k, idxTrain, idxTest, tied=True)
+    C_naive_arr = [C_arr[i] * np.identity(C_arr[i].shape[0]) for i in range(k)] # element by element mult.
+    return gaussianCSF(DTE, LTE, k, mu_arr, C_naive_arr, priorP, " Tied Naive Bayes", log, show)
+
+def K_fold_crossValidation(D, L, k, priorP, K, classifiers, seed=0):
+    nTest = int(D.shape[1] / K)
+    np.random.seed(seed)
+    idx = np.random.permutation(D.shape[1]) # take a random order of indexes from 0 to N
+    # idx = np.arange(D.shape[1]) # take a random order of indexes from 0 to N
+
+    error_acc = np.zeros((len(classifiers)))
+
+    for i in range(len(classifiers)):
+        startTest = 0
+        for j in range(K):
+            idxTest = idx[startTest: (startTest + nTest)] # take a different fold for test
+            # print(idxTest)
+            idxTrain = np.setdiff1d(idx, idxTest) # take as training set the remaining folds
+            # print(idxTrain)
+            error_acc[i] += classifiers[i](D, L, k, idxTrain, idxTest, priorP, log=False, show=False)
+            startTest += nTest
+    
+    error_rates = error_acc / D.shape[1] * 100
+    for i in range(len(classifiers)):
+        print(f'Classifier {i+1} error rate: {round(error_rates[i], 1)} %')
+
 
 def main():
     D, L = load('../datasets/iris.csv')
     priorP = vcol(np.array([1.0 / 3.0, 1.0 / 3.0, 1.0 / 3.0]))
     k = 3
 
-    (DTE, LTE), mu_arr, C_arr = classifierSetup(D, L, k, split_db_2to1)
+    # idxTrain, idxTest = split_db_2to1(D)
 
-    gaussianCSF(DTE, LTE, k, mu_arr, C_arr, priorP, "", log=True)
+    # gaussianCSF_wrapper(D, L, k, idxTrain, idxTest, priorP, log=False, show=True)
 
-    naiveBayesGaussianCSF(D, L, k, mu_arr, C_arr, priorP, log=False)
+    # naiveBayesGaussianCSF(D, L, k, idxTrain, idxTest, priorP, log=False, show=True)
 
-    tiedCovarianceGaussianCSF(D, L, k, split_db_2to1, priorP, log=True)
+    # tiedCovarianceGaussianCSF(D, L, k, idxTrain, idxTest, priorP, log=False, show=False)
 
+    # tiedNaiveBayesGaussianClassifier(D, L, k, idxTrain, idxTest, priorP, log=False, show=False)
+
+    CSF_list = [gaussianCSF_wrapper, naiveBayesGaussianCSF, tiedCovarianceGaussianCSF, tiedNaiveBayesGaussianClassifier]
+
+    K_fold_crossValidation(D, L, k, priorP, D.shape[1], CSF_list)
 
 
 if __name__ == "__main__":
