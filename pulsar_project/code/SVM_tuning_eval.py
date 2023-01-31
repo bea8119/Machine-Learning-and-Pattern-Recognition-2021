29d015c3e90406b5e6ecf9867c2bdf67a8b80caa
Bea <beatrice.piras99@gmail.com>
2023-01-31 16:19:59 +0100

    new img

--- pulsar_project/code/SVM_tuning_eval.py
+++ pulsar_project/code/SVM_tuning_eval.py
@@ -67,33 +67,33 @@ def main():
     p.plotDCFmin_vs_C_quadSVM_eval(C_arr, minDCF_arr, priorT_b, colors, application_points[0], c_list, saveFig=True)
 
     # ------------ RBF kernel SVM, tune C and gamma jointly, same appplication point (0.5, 1, 1) (unbalanced) ------
-    for m in PCA_list:
-        min_DCF_single = []
-        min_DCF_kfold = []
-        print('PCA m = {}'.format(m))
-        if m is not None:
-            DTR_PCA_fold = u.split_dataset(DTR, LTR, idxTrain, idxTest)[0][0] # Retrieve single fold train subset
-            PCA_Proj = f.PCA_givenM(DTR_PCA_fold, m) # Apply PCA over Training subset
-            DTR_PCA = np.dot(PCA_Proj.T, DTR) # Project both training and validation subsets with the output of the PC
-
-        for i, gamma in enumerate(gamma_list):
-            print('gamma = {}'.format(gamma))
-            min_DCF_single.append(np.array([]))
-            min_DCF_kfold.append(np.array([]))
-            for C in C_arr:
-                # ----------------- Using validation set (single fold or K-fold) ----------------------
-                # # Single Fold
-                # print('\tSingle Fold')
-                # min_DCF_single[i] = np.append(min_DCF_single[i], 
-                #     SVM.SVM_wrapper(DTR if m is None else DTR_PCA, LTR, K_svm, C, priorT_b[0], idxTrain, idxTest, application_points[0], show=False, kern=True, gamma=gamma, Poly_RBF=False)
-                # )
-
-                # K-fold
-                print('\tKfold')
-                min_DCF_kfold[i] = np.append(min_DCF_kfold[i], 
-                    SVM.K_fold_SVM(DTR, LTR, K, K_svm, C, priorT_b[0], application_points[0], m, show=True, kern=True, gamma=gamma, Poly_RBF=False, printStatus=True)
-                )
-        p.plotDCFmin_vs_C_RBFSVM(C_arr, None, min_DCF_kfold, m, n, K, colors, application_points[0], gamma_list)
+    #for m in PCA_list:
+    #    min_DCF_single = []
+    #    min_DCF_kfold = []
+    #    print('PCA m = {}'.format(m))
+    #    if m is not None:
+    #        DTR_PCA_fold = u.split_dataset(DTR, LTR, idxTrain, idxTest)[0][0] # Retrieve single fold train subset
+    #        PCA_Proj = f.PCA_givenM(DTR_PCA_fold, m) # Apply PCA over Training subset
+    #        DTR_PCA = np.dot(PCA_Proj.T, DTR) # Project both training and validation subsets with the output of the PC
+#
+    #    for i, gamma in enumerate(gamma_list):
+    #        print('gamma = {}'.format(gamma))
+    #        min_DCF_single.append(np.array([]))
+    #        min_DCF_kfold.append(np.array([]))
+    #        for C in C_arr:
+    #            # ----------------- Using validation set (single fold or K-fold) ----------------------
+    #            # # Single Fold
+    #            # print('\tSingle Fold')
+    #            # min_DCF_single[i] = np.append(min_DCF_single[i], 
+    #            #     SVM.SVM_wrapper(DTR if m is None else DTR_PCA, LTR, K_svm, C, priorT_b[0], idxTrain, idxTest, application_points[0], show=False, kern=True, gamma=gamma, Poly_RBF=False)
+    #            # )
+#
+    #            # K-fold
+    #            print('\tKfold')
+    #            min_DCF_kfold[i] = np.append(min_DCF_kfold[i], 
+    #                SVM.K_fold_SVM(DTR, LTR, K, K_svm, C, priorT_b[0], application_points[0], m, show=True, kern=True, gamma=gamma, Poly_RBF=False, printStatus=True)
+    #            )
+    #    p.plotDCFmin_vs_C_RBFSVM(C_arr, None, min_DCF_kfold, m, n, K, colors, application_points[0], gamma_list)
 
     plt.show()
 
